57db0fe722114d8484017cb120b1644e157c2bbb
cunnia3 <cunnia3@rpi.edu>
2013-06-13 14:37:15 -0500

    Started fleshing out the force field class. Currently, it is setup to allow for variable field shapes, but only has one field setup currently

--- AU_UAV_stack/AU_UAV_ROS/src/AU_UAV_ROS/ForceField.cpp
+++ AU_UAV_stack/AU_UAV_ROS/src/AU_UAV_ROS/ForceField.cpp
@@ -14,8 +14,24 @@ Date: 6/13/13
 
 #include "AU_UAV_ROS/ForceField.h"
 
-mathVector ForceField::findFieldForceMagnitude(Coordinates positionInField){
 
+	mathVector OvalField::findFieldForceMagnitude(Coordinates positionInField){
 
+	}
 
-}
+	bool OvalField::isCoordinatesInMyField(Coordinates positionInField, double fieldAngle){
+		int x = positionInField.x;
+		int y = positionInField.y;
+		if (fieldAngle > 90 && fieldAngle <270){
+			// plane generating the force is behind, therefore use the bottom boundary
+			double forceLimit = -sqrt((forceVars.gamma-(forceVars.alphaBot*pow(x,2)))/forceVars.betaBot);
+			if (y>forceLimit) return true;
+			else return false;
+		}
+		else{
+			// plane generating the force is in front, therefore use the top boundary
+			double forceLimit = sqrt((forceVars.gamma-(forceVars.alphaTop*pow(x,2)))/forceVars.betaTop);
+			if (y<forceLimit) return true;
+			else return false;
+		}
+	}
