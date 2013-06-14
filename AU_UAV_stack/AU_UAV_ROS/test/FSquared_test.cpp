eab57e8bd4a09df48b9e73a9bd3625de134f9e64
vptarmigan <victoria.chen.wu@gmail.com>
2013-06-14 16:22:46 -0500

    Testing file for fsquared init commit

---
+++ AU_UAV_stack/AU_UAV_ROS/test/FSquared_test.cpp
@@ -0,0 +1,58 @@
+/*
+ * fsquared_test
+ *
+ * let's test some stuff
+ * team 1 2013  
+ */
+
+#include <gtest/gtest.h>
+
+//what are we testing
+#include "AU_UAV_ROS/vmath.h"
+
+namespace	{
+class VectorStuff : public ::testing::Test	{
+
+	protected:
+		VectorStuff()	{
+			north.setDirection(90); north.setMagnitude(1);
+			east.setDirection(0); east.setMagnitude(1);
+			south.setDirection(270); south.setMagnitude(1);
+			west.setDirection(180); west.setMagnitude(1);
+		}
+	AU_UAV_ROS::mathVector north;
+	AU_UAV_ROS::mathVector east;
+	AU_UAV_ROS::mathVector south;
+	AU_UAV_ROS::mathVector west;
+
+};
+
+
+//find angle between two vectors
+TEST_F(VectorStuff, findingAngle)	{
+	int angleDiff;
+	//this greater angle than other when both are [0,360]
+	EXPECT_TRUE((angleDiff=south.findAngleBetween(north))==180||angleDiff==-180);
+	EXPECT_EQ(90, south.findAngleBetween(west));
+	EXPECT_EQ(-90, south.findAngleBetween(east));
+
+	//this smaller angle than other when both are [0,360]
+	EXPECT_TRUE((angleDiff=north.findAngleBetween(south))==180||angleDiff==-180);
+	EXPECT_EQ(-90, north.findAngleBetween(west));
+	EXPECT_EQ(90, north.findAngleBetween(east));
+
+	//sanity check
+	EXPECT_EQ(0, south.findAngleBetween(south));	
+}
+
+
+}
+
+
+
+
+int main(int argc, char **argv)	{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
